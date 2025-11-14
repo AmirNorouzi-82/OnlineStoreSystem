7a88a9df148721c2e07101fbe07dc2bd5e43fbd9
AmirNorouzi-82 <147101430+AmirNorouzi-82@users.noreply.github.com>
2025-11-14 20:01:42 +0330

    Update ApplicationServiceRegistration.cs and

--- src/Core/OnlineStoreSystem.Application/ApplicationServiceRegistration.cs
+++ src/Core/OnlineStoreSystem.Application/ApplicationServiceRegistration.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using System.Reflection;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace OnlineStoreSystem.Application;
@@ -8,6 +9,10 @@ public static class ApplicationServiceRegistration
     public static IServiceCollection RegisterApplicationServices(this IServiceCollection services,IConfiguration configuration)
     {
         // Register application services here
+        services.AddMediatR(options =>
+        {
+            options.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly());
+        });
         return services;
     }
 }
