bac5b1b489485e761676651364aa8af937401970
AmirNorouzi-82 <147101430+AmirNorouzi-82@users.noreply.github.com>
2025-11-14 20:12:52 +0330

    Add Logging

--- src/API/OnlineStoreSystem.API/Program.cs
+++ src/API/OnlineStoreSystem.API/Program.cs
@@ -1,20 +1,32 @@
 using OnlineStoreSystem.Application;
 using OnlineStoreSystem.Persistance;
+using Serilog;
+using OnlineStoreSystem.API.Middleware;
 
 var builder = WebApplication.CreateBuilder(args);
 
 
-builder.Services.AddControllers();
+Log.Logger = new LoggerConfiguration()
+    .ReadFrom.Configuration(builder.Configuration)
+    .Enrich.FromLogContext()
+    .WriteTo.Console()
+    .WriteTo.File("logs/log-.txt", rollingInterval: RollingInterval.Day)
+    .CreateLogger();
 
+builder.Host.UseSerilog();
+
+builder.Services.AddControllers();
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
 
 builder.Services.RegisterApplicationServices(builder.Configuration);
 builder.Services.RegisterPersistanceServices(builder.Configuration);
 
 var app = builder.Build();
+
+
+app.UseMiddleware<ExceptionHandlingMiddleware>();
 
 if (app.Environment.IsDevelopment())
 {
